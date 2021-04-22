95a6d7d0fb0a7e3f08882888c0f73aa9caf5148e
Avtar <prajapatiavtar@gmail.com>
2021-04-22 14:53:07 +0530

    added open graph

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,20 @@ export default function Page(props) {
       <Head>
         <title>E-commerce with AR</title>
         <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='Join Webinar on May 11, 2021 to Learn 1. Why active surveillance of COVID vaccine adverse events is important. 2. Why old ways of managing safety will not work in this COVID era.'
+        />
+        <meta property='og:title' content='Covid Vaccine Safety Webinar' />
+        <meta
+          property='og:description'
+          content='Join Webinar on May 11, 2021 to Learn 1. Why active surveillance of COVID vaccine adverse events is important. 2. Why old ways of managing safety will not work in this COVID era.'
+        />
+        <meta property='og:image' content='static/images/ecommerce.jpeg' />
+        <meta
+          property='og:url'
+          content='https://10xgrowth.com/covid-vaccine-safety'
+        />
       </Head>
       <Layout>
         <Home products={props.data} status={props.status} />
