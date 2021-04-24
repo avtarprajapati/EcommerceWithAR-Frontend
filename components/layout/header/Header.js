78f9fa73f4e5308dc31206c53a864dddb7e05d37
Avtar <prajapatiavtar@gmail.com>
2021-04-24 17:09:33 +0530

    remove unused Image import

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/Image';
 import { connect } from 'react-redux';
 import { useRouter } from 'next/router';
 import { makeStyles } from '@material-ui/core/styles';
