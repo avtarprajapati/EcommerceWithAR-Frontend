ae2dc2a2e976a15bd3baa8d261938f7bb2bfc9f6
Avtar <prajapatiavtar@gmail.com>
2021-04-24 15:29:10 +0530

    some minor changes

--- components/product-view/index.js
+++ components/product-view/index.js
@@ -23,8 +23,16 @@ function ProductView(props) {
   };
 
   const onCart = () => {
-    const product = userInfo.carts.findIndex((item) => item === data._id);
+    if (!userInfo?.cart) {
+      setSnackbar({
+        open: true,
+        message: 'Please login first',
+        severity: 'warning',
+      });
+      return false;
+    }
 
+    const product = userInfo.carts.findIndex((item) => item === data._id);
     if (product === -1) {
       addToCart(userInfo._id, { carts: [...userInfo.carts, data._id] });
       setSnackbar({
@@ -48,26 +56,32 @@ function ProductView(props) {
           <div className={styles.heading}>
             <h3>Product View Page</h3>
           </div>
-          <Grid container spacing={4}>
-            <Grid item lg={6}>
-              <model-viewer
-                height='500px'
-                src={data.imageUrl}
-                camera-controls
-                shadow-intensity='1'
-                ar
-                loading='lazy'
-                ar-modes='scene-viewer webxr quick-look'
-                camera-orbit='0.00001589deg 75deg 2.9m'
-              ></model-viewer>
+          <Grid container spacing={4} alignItems='center'>
+            <Grid item lg={6} sm={6} className={styles.product}>
+              <div>
+                <model-viewer
+                  height='500px'
+                  src={data.imageUrl}
+                  camera-controls
+                  shadow-intensity='1'
+                  ar
+                  loading='lazy'
+                  ar-modes='scene-viewer webxr quick-look'
+                  camera-orbit='0.00001589deg 75deg 2.9m'
+                ></model-viewer>
+              </div>
             </Grid>
-            <Grid item lg={6}>
-              <h2>{data.title}</h2>
-              <p>{data.description}</p>
-              <p>Rs. {data.price}</p>
-              <Button color='primary' variant='contained' onClick={onCart}>
-                Add To Cart
-              </Button>
+            <Grid item lg={6} sm={6}>
+              <div className={styles.info}>
+                <h2 className={styles.heading}>{data.title}</h2>
+                <p className={styles.description}>
+                  <b>Description: </b> {data.description}
+                </p>
+                <p className={styles.price}>Rs. {data.price}</p>
+                <Button color='primary' variant='contained' onClick={onCart}>
+                  Add To Cart
+                </Button>
+              </div>
               {/* <Button>Buy Now</Button> */}
             </Grid>
           </Grid>
